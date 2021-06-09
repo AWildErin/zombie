68f3169817c3bedf0cac458586d8c37a8b139e23
Erin <28122025+AWildErin@users.noreply.github.com>
2021-06-09 01:36:27 +0100

    Create ItemModule.cs

---
+++ code/Modules/ItemModule/ItemModule.cs
@@ -0,0 +1,15 @@
+namespace Zombie.Modules.ItemModule
+{
+	public partial class ItemModule : BaseModule
+	{
+		public override bool Init()
+		{
+			return true;
+		}
+
+		public override void Shutdown()
+		{
+			
+		}
+	}
+}
