22be83c00f76c71f0c430d8e0155c960e67a149d
Erin <28122025+AWildErin@users.noreply.github.com>
2021-06-09 14:56:52 +0100

    Fix Tip being Top and fix action being a string

--- code/Modules/ItemModule/Item/ItemFunction.cs
+++ code/Modules/ItemModule/Item/ItemFunction.cs
@@ -1,12 +1,14 @@
+using System;
+
 namespace Zombie.Modules.ItemModule
 {
 	public class ItemFunction
 	{
 		public string FunctionName { get; set; } = "";
-		public string FunctionTop { get; set; } = "";
+		public string FunctionTip { get; set; } = "";
 		public string FunctionIcon { get; set; } = "";
 
-		public string FunctionAction { get; set; }
+		public Action FunctionAction { get; set; }
 
 	}
 }
